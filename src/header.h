23754737530237fcabdf63de29ad77cbe51b12d5
Ryan Kane <rakane@syr.edu>
2019-09-30 09:00:36 -0400

    Added matrix allocate, input and print helper functions

--- src/header.h
+++ src/header.h
@@ -6,3 +6,5 @@ extern void matrix_add();
 extern void matrix_sub();
 extern void matrix_multiply();
 extern int** input_matrix(int, int);
+extern int** allocate_matrix(int, int);
+extern void print_matrix(int**, int, int);
--- src/matrix.c
+++ src/matrix.c
@@ -20,96 +20,69 @@ void matrix_rref() {
 void matrix_add() {
 
 	int rows, cols;
-	printf("How many rows in matrix 1?: ");
+	printf("How many rows in each matrix?: ");
 	scanf("%i", &rows);
-	printf("How many columns in matrix 1?: ");
-	scanf("%i", &cols);
-	
-	int ** arr1 = (int **) malloc(rows * sizeof(int *));
-	for(int i = 0; i < rows; i++) {
-		arr1[i] = (int *) malloc(cols * sizeof(int));	
-	}
-
-	arr1 = input_matrix(rows, cols);
-	
-	
-	printf("How many rows in matrix 2?: ");
-	scanf("%i", &rows);
-	printf("How many columns in matrix 2?: ");
+	printf("How many columns each matrix?: ");
 	scanf("%i", &cols);
 
-	int ** arr2 = (int **) malloc(rows * sizeof(int *));
-	for(int i = 0; i < rows; i++) {
-		arr2[i] = (int *) malloc(cols * sizeof(int));	
-	}
-
-	arr2 = input_matrix(rows, cols);
+	int** arr1 = input_matrix(rows, cols);
+	int** arr2 = input_matrix(rows, cols);
+	int** new_arr = allocate_matrix(rows, cols);	
 		
-	// TODO #1: Implement Logic Here
+	for(int i = 0; i < rows; i++) {
+		for(int j = 0; j < cols; j++) {
+			new_arr[i][j] = arr1[i][j] + arr2[i][j];
+		}
+	}
 
+	print_matrix(new_arr, rows, cols);
 }
 
 void matrix_sub() {
 	
 	int rows, cols;
-	printf("How many rows in matrix 1?: ");
+	printf("How many rows in each matrix?: ");
 	scanf("%i", &rows);
-	printf("How many columns in matrix 1?: ");
-	scanf("%i", &cols);
-	
-	
-	int ** arr1 = (int **) malloc(rows * sizeof(int *));
-	for(int i = 0; i < rows; i++) {
-		arr1[i] = (int *) malloc(cols * sizeof(int));	
-	}
-
-	arr1 = input_matrix(rows, cols);
-	
-	
-	printf("How many rows in matrix 2?: ");
-	scanf("%i", &rows);
-	printf("How many columns in matrix 2?: ");
+	printf("How many columns in each matrix?: ");
 	scanf("%i", &cols);
 
-	int ** arr2 = (int **) malloc(rows * sizeof(int *));
+	int** arr1 = input_matrix(rows, cols);
+	int** arr2 = input_matrix(rows, cols);
+	int** new_arr = allocate_matrix(rows, cols);
+
 	for(int i = 0; i < rows; i++) {
-		arr2[i] = (int *) malloc(cols * sizeof(int));	
+		for(int j = 0; j < cols; j++) {
+			new_arr[i][j] = arr1[i][j] - arr2[i][j];
+		}
 	}
 
-	arr2 = input_matrix(rows, cols);
-
-	// TODO #2: Implement Logic Here
-
-
+	print_matrix(new_arr, rows, cols);
+	
 }
 
 void matrix_multiply() {
-	int rows, cols;
+	int rows1, cols1, rows2, cols2;
 	printf("How many rows in matrix 1?: ");
-	scanf("%i", &rows);
+	scanf("%i", &rows1);
 	printf("How many columns in matrix 1?: ");
-	scanf("%i", &cols);
-	
-	
-	int ** arr1 = (int **) malloc(rows * sizeof(int *));
-	for(int i = 0; i < rows; i++) {
-		arr1[i] = (int *) malloc(cols * sizeof(int));	
-	}
+	scanf("%i", &cols1);
 
-	arr1 = input_matrix(rows, cols);
-	
+	int** arr1 = input_matrix(rows1, cols1);
 	
 	printf("How many rows in matrix 2?: ");
-	scanf("%i", &rows);
+	scanf("%i", &rows2);
 	printf("How many columns in matrix 2?: ");
-	scanf("%i", &cols);
+	scanf("%i", &cols2);
 
-	int ** arr2 = (int **) malloc(rows * sizeof(int *));
-	for(int i = 0; i < rows; i++) {
-		arr2[i] = (int *) malloc(cols * sizeof(int));	
+	int** arr2 = input_matrix(rows2, cols2);
+	
+	if(cols1 != rows2) {
+		printf("Invalid matrix dimensions");
+		return;
 	}
 
-	arr2 = input_matrix(rows, cols);
+
+	int** new_arr = allocate_matrix(rows1, cols2);
 
 	// TODO #3: Implement Logic Here
 
@@ -137,6 +110,25 @@ int** input_matrix(int rows, int columns) {
 	}
 
 	return arr;
+}
+int** allocate_matrix(int rows, int columns) {
+	int ** arr = (int **) malloc(rows * sizeof(int *));
+	for(int i = 0; i < rows; i++) {
+		arr[i] = (int *) malloc(columns * sizeof(int));	
+	}
+	return arr;
+}
+
+void print_matrix(int** arr, int rows, int columns) {
+	
+	printf("------New Matrix------\n");
+	for(int i = 0; i < rows; i++) {
+		for(int j = 0; j < columns; j++) {
+			printf("%i ", arr[i][j]); 
+		}
+		printf("\n");
+	}
+
 }
 
 
